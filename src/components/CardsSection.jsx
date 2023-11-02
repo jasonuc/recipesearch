c7422271d36dfc6346e5afeb753771da0ab39260
jasonuc <mrjasonu@gmail.com>
2023-11-02 21:02:51 +0000

    Update background color, border, and API error handling

--- src/components/CardsSection.jsx
+++ src/components/CardsSection.jsx
@@ -1,18 +1,11 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useState } from "react";
 import RecipeCard from "./RecipeCard";
 
 function CardsSection({ results }) {
-    const [renderedResults, setRenderedResults] = useState([]);
-    
-    useEffect(() => {
-        console.log("results: " + results)
-        setRenderedResults(results);
-    }, [results]);
 
     return (
         <div className='w-full flex-grow mt-3 flex flex-col items-center justify-stretch lg:flex-row lg:flex-wrap lg:justify-between gap-y-4 mb-2'>
-            {renderedResults && renderedResults.map( ({recipe}, index) =>
+            {results && results.map( ({recipe}, index) =>
                 <RecipeCard key={index} name={recipe.label} ingredients={recipe.ingredientLines} instructions={recipe.url} image={recipe.image} cuisineType={recipe.cuisineType} mealType={recipe.mealType} />
             )}
         </div>
--- src/services/RecipeSearch.js
+++ src/services/RecipeSearch.js
@@ -1,17 +1,18 @@
 const MY_APP_ID = import.meta.env.VITE_VERCEL_YOUR_APP_ID;
 const MY_APP_KEY = import.meta.env.VITE_VERCEL_YOUR_APP_KEY;
 
-async function getRecipesArray(mealName) {
-  const response = await fetch(`https://api.edamam.com/search?q=${mealName}&app_id=${MY_APP_ID}&app_key=${MY_APP_KEY}`)
-  const data = await response.json()
-  return data
-
-}
 
 async function fetchRecipes(mealName) {
-  const results = await getRecipesArray(mealName)
-  const { hits } = results
-  return hits
+
+  try {
+    const response = await fetch(`https://api.edamam.com/search?q=${mealName}&app_id=${MY_APP_ID}&app_key=${MY_APP_KEY}`)
+    const data = await response.json()
+    const { hits } = data
+    return hits
+  } catch (error) {
+    throw new Error("ERROR!")
+  }
+
 }
 
 export default fetchRecipes;

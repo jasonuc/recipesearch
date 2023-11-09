99f681a88aeb6b596ea10fec06f8a9621b0d987c
jasonuc <mrjasonu@gmail.com>
2023-11-09 10:37:16 +0000

    deacreses header height for mobile

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -13,7 +13,7 @@ function Title() {
     }
 
     return (
-        <div className=" snap-start border-b-2 border-b-terracotta border-opacity-10 w-screen h-24 pt-2 pb-5 shadow-md flex items-center justify-center sticky top-0 left-0 font-Agbalumo z-20 bg-alabaster">
+        <div className=" snap-start border-b-2 border-b-terracotta border-opacity-10 w-screen h-16 md:h-24 pt-2 pb-5 shadow-md flex items-center justify-center sticky top-0 left-0 font-Agbalumo z-20 bg-alabaster">
             <button onClick={handleClick}><h1 className=" text-3xl tracking-widest md:text-5xl font-bold text-center text-terracotta">Recipe Search</h1></button>
         </div>
     )

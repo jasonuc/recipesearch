00738528f4f7f4368145bc9c6e8b0d10183ac40d
jasonuc <mrjasonu@gmail.com>
2023-11-15 23:14:43 +0000

    corrects spelling

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -18,7 +18,7 @@ function Login({ setIsAuth }) {
 
   return (
     <div className=" w-[90%] md:w-[50%] my-5 h-[10rem] flex flex-col items-center justify-center rounded-md shadow-lg">
-      <h1 className="font-bold font-Agbalumo text-terracotta text-base md:text-2xl ">Sign In Using Exsisting Accounts</h1>
+      <h1 className="font-bold font-Agbalumo text-terracotta text-base md:text-2xl ">Sign In Using Existing Accounts</h1>
       <button onClick={userLogIn} className="flex text-terracotta h-12 w-[18rem] items-center justify-center border-2 rounded-md shadow-md active:shadow-sm active:scale-95 transition-all duration-300 ease-in-out mt-4 gap-x-3 font-bold font-sometypeMono">
         <FcGoogle size={25} />
         Sign In With Google
